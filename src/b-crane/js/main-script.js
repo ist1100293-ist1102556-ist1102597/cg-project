ee92135e8a566b0ab61b75d691e67ac5398cbda6
Carlos Felgueiras <catofel2000@gmail.com>
2024-05-08 22:33:30 +0100

    fix(crane): fixed hud font

--- src/b-crane/js/main-script.js
+++ src/b-crane/js/main-script.js
@@ -1,8 +1,5 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { VRButton } from 'three/addons/webxr/VRButton.js'
 import * as Stats from 'three/addons/libs/stats.module.js'
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
 
 //////////////////////
 /* GLOBAL VARIABLES */
